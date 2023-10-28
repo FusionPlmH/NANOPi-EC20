554f664a91bc04e4a90b5ff0831c6ea2b035a609
Fusion <leemenhin@gmail.com>
2023-10-29 05:21:55 +0800

    Update NetworkSwitch.sh

--- NetworkSwitch.sh
+++ NetworkSwitch.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-
 check_current_interface_1=$(route | grep '^default' | grep -o '[^ ]*$')
 check_current_interface_2=$(route | awk '/^default/{print $NF}')
 google_wired=$(ping -I eth0 -c 3 google.com | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
 baidu_wired=$(ping -I eth0 -c 3 baidu.com | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
 cloudflare_wired=$(ping -I eth0 -c 3 cloudflare.com | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
-
 google_modem=$(ping -I "ppp0" -c 3 google.com | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
 baidu_modem=$(ping -I "ppp0" -c 3 baidu.com | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
 cloudflare_modem=$(ping -I "ppp0" -c 3 cloudflare.com | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
-
-
-
 # Set Metric of different network
 ifmetric eth0 100
 ifmetric ppp0 110
-
-
 # Wired Network Connection Checking
 if [ $check_current_interface_1 == "eth0" || $check_current_interface_2 == "eth0" ]; then
   echo "Wired Network Selected"
@@ -33,7 +26,6 @@ if [ $check_current_interface_1 == "eth0" || $check_current_interface_2 == "eth0
 	route add default ppp0 
 	sleep 10s
   fi
-
 # Mobile Network Connection Checking
 elif [ $check_current_interface_1 == "ppp0" || $check_current_interface_2 == "ppp0" ]; then
   echo "Mobile Network Selected"
@@ -52,7 +44,6 @@ elif [ $check_current_interface_1 == "ppp0" || $check_current_interface_2 == "pp
 	route add default gw $default_route metric 0
 	sleep 10s
   fi
-	
 else
 	echo "Total Disconnected , wait 10 second to retry"
 	sleep 10s
