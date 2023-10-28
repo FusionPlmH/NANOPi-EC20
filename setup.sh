c2148d7482b980ea400ed8ce9d7e866af076ec38
Fusion <leemenhin@gmail.com>
2023-10-29 05:00:16 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@ sudo -s
 sed -i -e '# Startup 4G command' /etc/rc.local
 sed -i -e 'sleep 10s' /etc/rc.local
 sed -i -e 'sudo pppd call quectel-pppd' /etc/rc.local
-wget --no-check-certificate -t 1 -T 10 -q  https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-connect
-wget --no-check-certificate -t 1 -T 10 -q  https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-pppd
-wget --no-check-certificate -t 1 -T 10 -q  https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-disconnect
-sudo chmod a+x quectel-chat-connect
-sudo chmod a+x quectel-pppd
-sudo chmod a+x quectel-chat-disconnect
+wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-connect
+wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-pppd
+wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-disconnect
+sudo chmod a+x ./quectel-chat-connect
+sudo chmod a+x ./quectel-pppd
+sudo chmod a+x ./quectel-chat-disconnect
 systemctl enable rc-local
 reboot
