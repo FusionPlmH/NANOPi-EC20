3ac87e21bb3bb5f1af9068c1d825423a99f46088
Fusion <leemenhin@gmail.com>
2023-10-29 07:10:36 +0800

    Update ec20setup.sh

--- ec20setup.sh
+++ ec20setup.sh
@@ -1,6 +1,6 @@
 sudo -s
 sed -i -e '# Startup 4G command' /etc/rc.local
-sed -i -e 'sleep 10s' /etc/rc.local
+sed -i -e 'sleep 10\/s' /etc/rc.local
 sed -i -e 'sudo pppd call quectel-pppd' /etc/rc.local
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-connect
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-pppd
