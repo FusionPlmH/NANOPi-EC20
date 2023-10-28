66a1d3bc44cc3462c8f63241f3160f2500a13a95
Fusion <leemenhin@gmail.com>
2023-10-29 05:00:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ sed -i -e 'sudo pppd call quectel-pppd' /etc/rc.local
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-connect
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-pppd
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-disconnect
-sudo chmod a+x ./quectel-chat-connect
-sudo chmod a+x ./quectel-pppd
-sudo chmod a+x ./quectel-chat-disconnect
+sudo chmod a+x /etc/ppp/peers/quectel-chat-connect
+sudo chmod a+x /etc/ppp/peers/quectel-pppd
+sudo chmod a+x /etc/ppp/peers/quectel-chat-disconnect
 systemctl enable rc-local
 reboot
