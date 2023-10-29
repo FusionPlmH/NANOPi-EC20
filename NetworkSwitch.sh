ced1c547c2473b7e5e21f7c7d565411e5bd3d8ac
Fusion <leemenhin@gmail.com>
2023-10-29 21:31:05 +0800

    Update NetworkSwitch.sh

--- NetworkSwitch.sh
+++ NetworkSwitch.sh
@@ -9,15 +9,19 @@ ali_modem=$(ping -I eth0 -c 3 223.5.5.5 | grep 'received' | awk '{print $4}' | c
 cloudflare_modem=$(ping -I "ppp0" -c 3 1.1.1.1 | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
 
 # Wired Mobile Network Connection Checking
-if [[ $check_current_interface_1 -eq "eth0" || $check_current_interface_2 -eq "eth0" ] | [ $google_wired -eq 3 || $ali_wired -eq 3 || $cloudflare_wired -eq 3 ]]; then
-	echo "Wired External Network connect Successfully and in use , auto check it again later" >> /etc/networkswitch.log
- 	ifmetric eth0 0
-	ifmetric ppp0 100
+if [[ $check_current_interface_1 -eq "eth0" || $check_current_interface_2 -eq "eth0" ]] ; then
+	if [[ $google_wired -eq 3 || $ali_wired -eq 3 || $cloudflare_wired -eq 3 ]]; then
+		echo "Wired External Network connect Successfully and in use , auto check it again later" >> /etc/networkswitch.log
+ 		ifmetric eth0 0
+		ifmetric ppp0 100
+  	fi
 else
-	if [[ $google_modem -eq 3 || $ali_modem -eq 3 || $cloudflare_modem -eq 3 ] | [ $google_wired -ne 3 | $ali_wired -ne 3 | $cloudflare_wired -ne 3 ]]; then
-       		echo "Mobile External Network connect Successfully and in use , Fix Wired Network Connection ASAP! " >> /etc/networkswitch.log
-	   	ifmetric eth0 100
-		ifmetric ppp0 0
+	if [[ $google_modem -eq 3 || $ali_modem -eq 3 || $cloudflare_modem -eq 3 ] ; then
+ 		if [[ $google_wired -ne 3 | $ali_wired -ne 3 | $cloudflare_wired -ne 3 ]]; then
+       			echo "Mobile External Network connect Successfully and in use , Fix Wired Network Connection ASAP! " >> /etc/networkswitch.log
+	   		ifmetric eth0 100
+			ifmetric ppp0 0
+   		fi
 	fi
 fi
 echo "Complete Time: $(date) , will run this script 5 second later " >> /etc/networkswitch.log
