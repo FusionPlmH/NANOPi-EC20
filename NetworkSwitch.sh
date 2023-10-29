3c289c3a1c6a27f572955bd2658b94d99314c620
Fusion <leemenhin@gmail.com>
2023-10-29 20:48:36 +0800

    Update NetworkSwitch.sh

--- NetworkSwitch.sh
+++ NetworkSwitch.sh
@@ -13,8 +13,6 @@ if [[ $check_current_interface_1 == "eth0" || $check_current_interface_2 == "eth
   echo "Wired Network Detected" >> /etc/networkswitch.log
   if [[ $google_wired == 3 || $ali_wired == 3 || $cloudflare_wired == 3 ]]; then
 	echo "Wired External Network connect Successfully , auto check it again later" >> /etc/networkswitch.log
- 	ifmetric ppp0 0
-       	ifmetric eth0 100
   else
     	echo "External Network Unreachable ， Checking Mobile Network Status" >> /etc/networkswitch.log
      	if [[ $check_current_interface_1 == "ppp0" || $check_current_interface_2 == "ppp0" ]]; then
@@ -22,9 +20,13 @@ if [[ $check_current_interface_1 == "eth0" || $check_current_interface_2 == "eth
   		if [[ $google_modem == 3 || $ali_modem == 3 || $cloudflare_modem == 3 ]]; then
 			echo "Mobile External Network Connect Successfully , Check Wired Network" >> /etc/networkswitch.log
   			if [[ $google_wired == 3 || $ali_wired == 3 || $cloudflare_wired == 3 ]]; then
-    				echo "Wired External Network Connected , Switching Back" >> /etc/networkswitch.log
+    				echo "Wired External Network Connected , Cet Connet Back to Wired Network" >> /etc/networkswitch.log
 				ifmetric eth0 0
 				ifmetric ppp0 100
+    			else 
+       				echo "Mobile Network in Use , Fix Wired Network Connection ASAP! " >> /etc/networkswitch.log
+	   			ifmetric eth0 100
+				ifmetric ppp0 0
    			fi
   		fi
 	fi
