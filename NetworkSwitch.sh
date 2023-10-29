2de8af37e7d775a978f5cba043ca1a56e46584ff
Fusion <leemenhin@gmail.com>
2023-10-29 21:02:03 +0800

    Update NetworkSwitch.sh

--- NetworkSwitch.sh
+++ NetworkSwitch.sh
@@ -9,28 +9,16 @@ ali_modem=$(ping -I eth0 -c 3 223.5.5.5 | grep 'received' | awk '{print $4}' | c
 cloudflare_modem=$(ping -I "ppp0" -c 3 1.1.1.1 | grep 'received' | awk '{print $4}' | cut -d '/' -f 1)
 
 # Wired Mobile Network Connection Checking
-if [[ $check_current_interface_1 == "eth0" || $check_current_interface_2 == "eth0" ]]; then
-  echo "Wired Network Detected" >> /etc/networkswitch.log
-  if [[ $google_wired == 3 || $ali_wired == 3 || $cloudflare_wired == 3 ]]; then
-	echo "Wired External Network connect Successfully , auto check it again later" >> /etc/networkswitch.log
-  else
-    	echo "External Network Unreachable ， Checking Mobile Network Status" >> /etc/networkswitch.log
-     	if [[ $check_current_interface_1 == "ppp0" || $check_current_interface_2 == "ppp0" ]]; then
-  		echo "Mobile Network Detected" >> /etc/networkswitch.log
-  		if [[ $google_modem == 3 || $ali_modem == 3 || $cloudflare_modem == 3 ]]; then
-			echo "Mobile External Network Connect Successfully , Check Wired Network" >> /etc/networkswitch.log
-  			if [[ $google_wired == 3 || $ali_wired == 3 || $cloudflare_wired == 3 ]]; then
-    				echo "Wired External Network Connected , Cet Connet Back to Wired Network" >> /etc/networkswitch.log
-				ifmetric eth0 0
-				ifmetric ppp0 100
-    			else 
-       				echo "Mobile Network in Use , Fix Wired Network Connection ASAP! " >> /etc/networkswitch.log
-	   			ifmetric eth0 100
-				ifmetric ppp0 0
-   			fi
-  		fi
+if [[ $check_current_interface_1 == "eth0" || $check_current_interface_2 == "eth0" ] | [ $google_wired == 3 || $ali_wired == 3 || $cloudflare_wired == 3 ] ]; then
+	echo "Wired External Network connect Successfully and in use , auto check it again later" >> /etc/networkswitch.log
+ 	ifmetric eth0 0
+	ifmetric ppp0 100
+else
+	if [[ $google_modem == 3 || $ali_modem == 3 || $cloudflare_modem == 3 ] | [ $google_wired != 3 | $ali_wired != 3 | $cloudflare_wired != 3 ] ]; then
+       		echo "Mobile External Network connect Successfully and in use , Fix Wired Network Connection ASAP! " >> /etc/networkswitch.log
+	   	ifmetric eth0 100
+		ifmetric ppp0 0
 	fi
-  fi
 fi
 echo "Complete Time: $(date) , will run this script 5 second later " >> /etc/networkswitch.log
 sleep 2s
