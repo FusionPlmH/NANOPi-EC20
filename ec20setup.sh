d58e154407fa97686991b240fa3eb892d0f7c4e5
Fusion <leemenhin@gmail.com>
2025-11-16 01:00:01 +0800

    Update ec20setup.sh

--- ec20setup.sh
+++ ec20setup.sh
@@ -1,13 +1,8 @@
-sed -i '/exit 0/d' /etc/rc.local
-echo "# Startup 4G command" >> /etc/rc.local
-echo "sleep 10s" >> /etc/rc.local
-echo "sudo pppd call quectel-pppd" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-connect
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-pppd
 wget --no-check-certificate -t 1 -T 10 -q -P /etc/ppp/peers https://raw.githubusercontent.com/FusionPlmH/NANOPi-EC20/main/quectel-chat-disconnect
 sudo chmod a+x /etc/ppp/peers/quectel-chat-connect
 sudo chmod a+x /etc/ppp/peers/quectel-pppd
 sudo chmod a+x /etc/ppp/peers/quectel-chat-disconnect
-systemctl enable rc-local
+systemctl enable ec20-lte.service
 reboot
